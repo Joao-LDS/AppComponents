75fccc7a1fe2b47d6b1d3e509a5868d38fce7608
Joao Luis dos Santos <joao.luis-terceiro@cea.com.br>
2021-12-21 13:40:30 -0300

    Criação do target SandBox e podspec

---
+++ AppComponents/AppComponents.h
@@ -0,0 +1,18 @@
+//
+//  AppComponents.h
+//  AppComponents
+//
+//  Created by TLSP-000092 on 21/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppComponents.
+FOUNDATION_EXPORT double AppComponentsVersionNumber;
+
+//! Project version string for AppComponents.
+FOUNDATION_EXPORT const unsigned char AppComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppComponents/PublicHeader.h>
+
+
---
+++ SandBox/SandBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandBox
+//
+//  Created by TLSP-000092 on 21/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
